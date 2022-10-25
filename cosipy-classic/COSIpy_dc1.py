1cabe0ad1e7cf1a1a2dfa64fbbc6928ddd15b589
Christopher Karwin <ckarwin@login004.palmetto.clemson.edu>
2022-10-25 19:11:00 -0400

    fixed a couple small bugs with crab balloon imaging

--- cosipy-classic/COSIpy_dc1.py
+++ cosipy-classic/COSIpy_dc1.py
@@ -1114,7 +1114,7 @@ class BG():
         # elif self.bg_mode == 'sim 6deg despina':
         if self.bg_mode == 'default 6deg':
             print('Reading in flight-average background response for 6 deg CDS binning ...')
-            self.default_bg_response_file = 'flight_bg_all_v1_fine_6deg.npz'
+            self.default_bg_response_file = '../../data_products/flight_bg_all_v1_fine_6deg.npz'
         
         if self.bg_mode == 'sim 6deg despina':
             print('Reading in simulated Ling-model (1973) background response for 6 deg CDS binning from despina only...')
